9e0c8dece4c1b141665426d88e068a45e5e790d8
SteveDok22 <s.doktorov123@gmail.com>
2025-07-28 19:15:25 +0200

    modified:   Assets/script.js

--- Assets/script.js
+++ Assets/script.js
@@ -2,13 +2,17 @@
 const QRCode = require('qrcode');
 
 // Generate QR code
-QRCode.toDataURL('https://stevedok22.github.io/Project-CV-Code-0.2/', {
+QRCode.toDataURL('resume=qr-code.png', 'https://stevedok22.github.io/Project-CV-Code-0.2/', {
     width: 300,
     margin: 2,
     color: {
         dark: '#000000',
         light: '#FFFFFF'
     }
-}).then(url => {
-    console.log(url); // Base64 string with image
+}, (err) => {
+    if (err) {
+        console.log('Error:', err);
+    } else {
+        console.log('QR code saved successfully!');
+    }
 });
