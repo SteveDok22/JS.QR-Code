eeb2bdf5a7254d82d9b002d2b97bbf5b8f3d2edb
SteveDok22 <s.doktorov123@gmail.com>
2025-08-10 17:20:45 +0200

    Adderd and modified:   Assets/js/script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -42,3 +42,39 @@ class ThunderQRGenerator {
             throw new Error('QR generation failed: ' + error.message);
         }
     }
+
+    async generateWithGoogleAPI() {
+        try {
+            const encodedURL = encodeURIComponent(this.url);
+            const qrURL = `https://chart.googleapis.com/chart?cht=qr&chs=${this.size}x${this.size}&chl=${encodedURL}&choe=UTF-8`;
+
+            return new Promise((resolve, reject) => {
+                const img = new Image();
+                img.crossOrigin = 'anonymous';
+
+                img.onload = () => {
+                    const canvas = document.createElement('canvas');
+                    canvas.width = this.size;
+                    canvas.height = this.size;
+                    const ctx = canvas.getContext('2d');
+
+                    ctx.fillStyle = '#FFFFFF';
+                    ctx.fillRect(0, 0, this.size, this.size);
+                    ctx.drawImage(img, 0, 0, this.size, this.size);
+
+                    this.canvas = canvas;
+                    this.dataURL = canvas.toDataURL('image/png');
+
+                    resolve(canvas);
+                };
+
+                img.onerror = () => {
+                    reject(new Error('Failed to load QR code from Google Charts API'));
+                };
+
+                img.src = qrURL;
+            });
+        } catch (error) {
+            throw new Error('QR generation failed: ' + error.message);
+        }
+    }
